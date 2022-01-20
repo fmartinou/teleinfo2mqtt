e67524fb75aa1f569cc9090ee46541647c7a2438
Thyphus <jeanalex.janvier@gmail.com>
2022-01-20 19:56:52 +0100

    FIx bug

--- app/teleinfo/index.js
+++ app/teleinfo/index.js
@@ -17,52 +17,52 @@ function isSameFrame(frame1, frame2) {
 //check all data which can be received by standard mode
 function CheckStandardData(lineItems)
 {
-	switch (lineItems[0]) 
+    switch (lineItems[0]) 
     {
-	case 'ADSC':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 12)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'ADSC':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 12)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'VTIC':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'VTIC':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'DATE':
-	{
-	    if(lineItems.length >= 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'DATE':
+    {
+        if(lineItems.length >= 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'NGTF':
-	case 'LTARF':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 16)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'NGTF':
+    case 'LTARF':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 16)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
     case 'EAST':
     case 'EASF01':
@@ -80,62 +80,62 @@ function CheckStandardData(lineItems)
     case 'EASD03':
     case 'EASD04':
     case 'EAIT':
-	case 'ERQ1':
+    case 'ERQ1':
     case 'ERQ2':
     case 'ERQ3':
     case 'ERQ4':
-	case 'LTARF':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 9)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'LTARF':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 9)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'IRMS1':
+    case 'IRMS1':
     case 'IRMS2':
     case 'IRMS3':
-	case 'URMS1':
+    case 'URMS1':
     case 'URMS2':
     case 'URMS3':
     {
-	    if(lineItems.length >= 2 && lineItems[1].length == 3)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+        if(lineItems.length >= 2 && lineItems[1].length == 3)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
     case 'PREF':
     case 'PCOUP':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
     case 'SINSTS':
     case 'SINSTS1':
     case 'SINSTS2':
     case 'SINSTS3':
     {
-	    if(lineItems.length >= 2 && lineItems[1].length == 5)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+        if(lineItems.length >= 2 && lineItems[1].length == 5)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }   
     case 'SMAXSN':
     case 'SMAXSN1':
@@ -145,26 +145,26 @@ function CheckStandardData(lineItems)
     case 'SMAXSN1-1':
     case 'SMAXSN2-1':
     case 'SMAXSN3-1':
-	{
-	    if(lineItems.length >= 3 && lineItems[2].length == 5)
-	    {
-	 	   return 2;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    {
+        if(lineItems.length >= 3 && lineItems[2].length == 5)
+        {
+           return 2;
+        }
+        else
+        {
+           return -1;
+        }
     } 
     case 'SINSTI':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 5)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 5)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     } 
     case 'SMAXIN':  
     case 'SMAXIN-1':
@@ -172,117 +172,117 @@ function CheckStandardData(lineItems)
     case 'CCASN-1':
     case 'CCAIN':
     case 'CCAIN-1':
-	{
-	    if(lineItems.length >= 3 && lineItems[2].length == 5)
-	    {
-	 	   return 2;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    {
+        if(lineItems.length >= 3 && lineItems[2].length == 5)
+        {
+           return 2;
+        }
+        else
+        {
+           return -1;
+        }
     } 
-	case 'UMOY1':
+    case 'UMOY1':
     case 'UMOY2':
     case 'UMOY3':
-	{
-	    if(lineItems.length >= 3 && lineItems[2].length == 3)
-	    {
-	 	   return 2;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    {
+        if(lineItems.length >= 3 && lineItems[2].length == 3)
+        {
+           return 2;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'STGE':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 8)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'STGE':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 8)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'DPM1':
-	case 'FPM1':
-	case 'DPM2':
-	case 'FPM2':
-	case 'DPM3':
-	case 'FPM3':
-	{
-	    if(lineItems.length >= 3 && lineItems[2].length == 2)
-	    {
-	 	   return 2;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'DPM1':
+    case 'FPM1':
+    case 'DPM2':
+    case 'FPM2':
+    case 'DPM3':
+    case 'FPM3':
+    {
+        if(lineItems.length >= 3 && lineItems[2].length == 2)
+        {
+           return 2;
+        }
+        else
+        {
+           return -1;
+        }
     }
     //MSG1
-	//MSG2
-	case 'PRM':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 14)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    //MSG2
+    case 'PRM':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 14)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'RELAIS':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 3)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'RELAIS':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 3)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'NTARF':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'NTARF':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'NJOURF':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'NJOURF':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	case 'NJOURF+1':
-	{
-	    if(lineItems.length >= 2 && lineItems[1].length == 2)
-	    {
-	 	   return 1;
-	    }
-	    else
-	    {
-	 	   return -1;
-	    }
+    case 'NJOURF+1':
+    {
+        if(lineItems.length >= 2 && lineItems[1].length == 2)
+        {
+           return 1;
+        }
+        else
+        {
+           return -1;
+        }
     }
-	//PJOURF+1
-	//PPOINTE
-	}
-	return -1;
+    //PJOURF+1
+    //PPOINTE
+    }
+    return -1;
 }
 
 /**
@@ -295,31 +295,31 @@ function processData(data, teleInfoEventEmitter) {
 
     // Split line `${label} ${value} ${checksum}
     const lineItems = dataStr.split(/\s+/);
-	
-	if (lineItems.length < 3) {
+    
+    if (lineItems.length < 3) {
         log.error(`Corrupted data received [${dataStr}]`);
         return;
     }
-	
-	var dataIndex = 1;
-	if(ticMode === 'HISTORY')
-	{
-		
-	}
-	else
-	{
-		dataIndex = CheckStandardData(lineItems);
-	}
-	
-	if(dataIndex == -1)
-	{
+    
+    var dataIndex = 1;
+    if(ticMode === 'HISTORY')
+    {
+        
+    }
+    else
+    {
+        dataIndex = CheckStandardData(lineItems);
+    }
+    
+    if(dataIndex == -1)
+    {
         return;
-	}
+    }
 
     const label = lineItems[0];
 
     // Frame end? -> Dispatch frame event
-    if ((ticMode === 'HISTORY' && label === 'MOTDETAT' && currentFrame.ADCO) || (ticMode === 'STANDARD' && label === 'NJOURF+1' && currentFrame.ADSC))
+    if ((ticMode === 'HISTORY' && label === 'MOTDETAT' && currentFrame.ADCO) || (ticMode === 'STANDARD' && label === 'ADSC' && currentFrame.ADSC))
     {
         if (!isSameFrame(currentFrame, previousFrame)) 
         {
@@ -386,7 +386,7 @@ async function connect() {
                 log.error(`Error when connecting to serial port [${error.message}]`);
                 reject(error);
             } else {
-                const parser = serialPort.pipe(new Readline({ delimiter: '\r\n' }));
+                const parser = serialPort.pipe(new Readline());
 
                 const teleInfoEventEmitter = new events.EventEmitter();
 
