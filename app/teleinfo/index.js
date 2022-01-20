4d1f3d90e48bfdeaa66b276e0a3710732fa7ac88
Thyphus <jeanalex.janvier@gmail.com>
2022-01-20 10:48:02 +0100

    Fix bug

--- app/teleinfo/index.js
+++ app/teleinfo/index.js
@@ -69,7 +69,7 @@ function processData(data, teleInfoEventEmitter) {
         indexUtil = 2;
     }
     
-    if(lineItems.length<=4)
+    if(lineItems.length<=5)
     {
         // Sanitize value & try to convert to number
         const valueSanitized = lineItems[indexUtil].replace(/\.\./g, '');
